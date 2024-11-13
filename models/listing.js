6beff8bbdf1303d9c0b13a556704ae1267e4565b
Nupur Aidban <nupuraidban2002@gmail.com>
2024-11-14 00:54:26 +0530

    Implemented MVC Architecture & Image Upload

--- models/listing.js
+++ models/listing.js
@@ -9,9 +9,9 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
-        type: String,  // Store image as a string (URL)
-        default: "https://plus.unsplash.com/premium_photo-1666900440561-94dcb6865554?q=80&w=1854&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",  // Default image URL
-        set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1666900440561-94dcb6865554?q=80&w=1854&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v // If empty, use default URL
+        filename: String,
+        url: String,
+       
     },
     price: Number,
     location: String,
